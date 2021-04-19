a736b3a28ae875dd16f823d687339c849751f860
Oleg Cherbaiev <oleg.cherbaieff@gmail.com>
2021-04-19 23:01:57 +0300

    prod

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,11 @@
-$(document).ready(function () {
+jQuery(document).ready(function ($) {
    $('.carousel__inner').slick({
       dots: true,
       speed: 300,
       prevArrow:
-         '<button type="button" class="slick-prev arrow"><img src="/assets/images/slider_parts/prev-slide.webp"></button>',
+         '<button type="button" class="slick-prev arrow"><img src="assets/images/slider_parts/prev-slide.webp"></button>',
       nextArrow:
-         '<button type="button" class="slick-next arrow"><img src="/assets/images/slider_parts/next-slide.webp"></button>',
+         '<button type="button" class="slick-next arrow"><img src="assets/images/slider_parts/next-slide.webp"></button>',
       responsive: [
          {
             breakpoint: 600,
