eb03a82de666f9291c2fcb4723fff6ed5c39b01f
Oleg Cherbaiev <oleg.cherbaieff@gmail.com>
2021-04-19 22:45:33 +0300

    Upd

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+   $('.carousel__inner').slick({
+      dots: true,
+      speed: 300,
+      prevArrow:
+         '<button type="button" class="slick-prev arrow"><img src="/assets/images/slider_parts/prev-slide.webp"></button>',
+      nextArrow:
+         '<button type="button" class="slick-next arrow"><img src="/assets/images/slider_parts/next-slide.webp"></button>',
+      responsive: [
+         {
+            breakpoint: 600,
+            settings: {
+               arrows: false,
+               dots: true,
+            },
+         },
+      ],
+   });
+});
